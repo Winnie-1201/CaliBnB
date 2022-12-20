17827deb97c7e0c559cd8a2e7068f279ba41140a
Winnie <luwinnie12@gmail.com>
2022-12-20 11:51:15 -0500

    update booking route

--- app/api/booking_routes.py
+++ app/api/booking_routes.py
@@ -4,3 +4,46 @@ from app.models import db, Spot, Review, Booking
 from app.forms import SpotForm, ReviewForm, BookingForm
 
 booking_routes = Blueprint('bookings', __name__)
+
+@booking_routes.route('/current')
+@login_required
+def user_bookings():
+    '''
+    Query for current user's bookings and return them in a list of dictionaries
+    '''
+
+    bookings = Booking.query.filter_by(userId=current_user.id).all()
+
+    return {'Bookings', [booking.to_dict() for booking in bookings]}
+
+
+
+@booking_routes.route('/<bookingId>', methods=['PUT'])
+@login_required
+def edit_booking(bookingId):
+    '''
+    Query for editting an existing booking and return it in a dictionary
+    '''
+    booking = Booking.query.get(bookingId)
+
+    form = BookingForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit:
+        if booking.userId == current_user.id:
+            booking['start'] = form.data['start']
+            booking['end'] = form.data['end']
+
+            db.session.commit()
+            return booking.to_dict()
+        
+        else:
+            return {'error': 'You do not have access'}
+
+    if form.errors:
+        return form.errors
+
+
+
+
+
