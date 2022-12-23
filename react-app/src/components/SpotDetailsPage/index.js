9295de57558da3819f30f160c45275b2a5afb6e6
Winnie <luwinnie12@gmail.com>
2022-12-22 19:00:59 -0500

    part two

--- react-app/src/components/SpotDetailsPage/index.js
+++ react-app/src/components/SpotDetailsPage/index.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Homepage/Header";
 import PartOne from "./PartOne";
 import "./index.css";
+import PartTwo from "./PartTwo";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { getOneSpotThunk } from "../../store/spots";
 
 function SpotDetailsPage() {
+  const dispatch = useDispatch();
+  const { spotId } = useParams();
+
+  const [isLoaded, setLoaded] = useState(false);
+
+  const spotDetail = useSelector((state) => state.spots.singleSpot);
+
+  useEffect(() => {
+    dispatch(getOneSpotThunk(spotId)).then(() => setLoaded(true));
+  }, [dispatch]);
+
   return (
-    <>
-      <Header />
-      <main className="site-content">
-        <PartOne />
-      </main>
-    </>
+    isLoaded && (
+      <>
+        <Header />
+        <main className="site-content">
+          <PartOne spot={spotDetail} />
+          <PartTwo spot={spotDetail} />
+        </main>
+      </>
+    )
   );
 }
 
